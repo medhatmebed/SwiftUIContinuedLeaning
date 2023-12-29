d68296990f1070e928137a289e7808e9c955e3ac
medhatmebed <medhat.mebed@gmail.com>
2023-12-29 06:02:52 +0200

    Initial Commit

---
+++ SwiftUIContinuedLeaning/SwiftUIContinuedLeaningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIContinuedLeaningApp.swift
+//  SwiftUIContinuedLeaning
+//
+//  Created by Medhat Mebed on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIContinuedLeaningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
